47bf2b044fa3f4671ef10ccf0030edae3c5a07cc
Yoonji Choi <yoonji110@dankook.ac.kr>
2024-05-16 16:25:06 +0900

    DB changed

--- mytodolist/mytodolist/settings.py
+++ mytodolist/mytodolist/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'mytodolist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'todolist',
+        'USER': 'mariauser',
+        'PASSWORD': 'mariapassword',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
